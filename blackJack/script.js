43f410ab16915805e0113da82bdbb408995a50cb
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-07-08 17:00:37 -0500

    JavaScript

--- blackJack/script.js
+++ blackJack/script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+
+// console.log(sum);
+
+if (sum <= 20) {
+  console.log("Do you want another card?");
+} else if (sum === 21) {
+  console.log("You have black Jack!");
+  hasBlackJack = true;
+} else {
+  console.log("Game over!");
+}
+
+// Cash Out
+console.log(hasBlackJack);
